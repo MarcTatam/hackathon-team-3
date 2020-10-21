4eb2174a760151dad27322941f33502cbb9080c8
Marc Tatam <marc.smsjb.tatam@gmail.com>
2020-10-21 19:41:48 +0100

    last commit

--- finalebuttons.py
+++ finalebuttons.py
@@ -1,11 +1,23 @@
 import pygame
 
-myfont = pygame.font.SysFont('Arial', 20)
+#myfont = pygame.font.SysFont('Arial', 20)
 class done:
+    coordinate_x = 0
+    coordinate_y = 0
+    text = ""
     def __init__(self, x: int, y: int, text: str, surface: pygame.surface):
         this_rect = pygame.Rect((x,y),(50,50))
         pygame.draw.rect(surface, (0, 255, 0, 255), this_rect)
-class tasknames:
-    def __init__(self, x: int, y: int, text: str):
-        self.textsurface = myfont.render(text, False, (0, 0, 0))
+        self.coordinate_x = x
+        self.coordinate_y = y
+        self.text = text
+    def is_location(coord,otherarg)->bool:
+        if coordinate_x <= cord[0] <= coordinate_x + 50 and coordinate_y <= cord[1] <= coordinate_y + 50:
+            return False
+        else:
+            return True
+
+#class tasknames:
+    #def __init__(self, x: int, y: int, text: str):
+     #   self.textsurface = myfont.render(text, False, (0, 0, 0))
 
--- finalefrontend.py
+++ finalefrontend.py
@@ -1,8 +1,11 @@
 import pygame
 import finalebuttons
 import finale_functions
+
 pygame.init()
-for i in range(0,40):
+pygame.font.init()
+
+for i in range(0,5):
     finale_functions.add_task("test","test")
 button_array = []
 screen = pygame.display.set_mode([1280, 720])
@@ -10,17 +13,24 @@ def main():
     '''Runs the main GUI loop'''
     running = True
     while running:
-        tasks = finale_functions.read_tasks()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
         screen.fill((255, 255, 255))
-        task_list_length = len(tasks)
         current_y = 10
+        tasks = finale_functions.read_tasks()
         for task in tasks:
             button_array.append(finalebuttons.done(700, current_y, "", screen))
             current_y += 60
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                for button in button_array:
+                    print(pos)
+                    if button.is_location(pos):
+                        finale_functions.delete_task(int(button.text))
+        task_list_length = len(tasks)
         pygame.display.flip()
+        button_array.clear()
 
 if __name__ == "__main__":
     main()
