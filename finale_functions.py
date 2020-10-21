40994680d2a07a90292cb7d13105d0a60439f91f
Leon <68304209+leoningall@users.noreply.github.com>
2020-10-21 19:17:18 +0100

    Add files via upload

--- finale_functions.py
+++ finale_functions.py
@@ -1,15 +1,28 @@
+import json
+
+json_file = open('tasks.json','w')
+
 tasks = {}
 id_counter = 0
 
 def add_task(task,description):
     global id_counter
+    global json_tasks
     id_counter += 1
     tasks[id_counter] = [task, description]
+    print(f'writing {tasks} to file...')
+    json_file.write(json.dumps(tasks))
 
 def delete_task(task):
+    json_file = open('tasks.json','w')
     for i in range(1,len(tasks)+1):
         if tasks[i][0] == task:
             tasks.pop(i)
+    print(f'writing {tasks} to json file...')
+    json_file.write(json.dumps(tasks))
+        
 
 def read_tasks():
     return tasks
+
+json_file.close()
