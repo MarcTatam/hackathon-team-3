a93dc67939d47ac2ddb93797006a44fec181315c
Marc Tatam <marc.smsjb.tatam@gmail.com>
2020-10-21 18:59:44 +0100

    Add inital front end

--- finale_functions.py
+++ finale_functions.py
@@ -1,32 +1,8 @@
-<<<<<<< Updated upstream
-import json
-
-=======
->>>>>>> Stashed changes
 tasks = {}
 id_counter = 0
 
 def add_task(task,description):
     global id_counter
-<<<<<<< Updated upstream
-    global json_tasks
-    id_counter += 1
-    tasks[id_counter] = [task, description]
-    json_tasks = json.dumps(tasks)
-
-def delete_task(task):
-    global json_tasks
-    for i in range(1,len(tasks)+1):
-        if tasks[i][0] == task:
-            tasks.pop(i)
-    json_tasks = json.dumps(tasks)
-
-def read_tasks():
-    return tasks
-
-add_task('hello','goodbye')
-print(json_tasks)
-=======
     id_counter += 1
     tasks[id_counter] = [task, description]
 
@@ -37,4 +13,3 @@ def delete_task(task):
 
 def read_tasks():
     return tasks
->>>>>>> Stashed changes
